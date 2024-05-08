c63d9e653d1ff67a0f9166271886157a8c9b913a
CandersA <123170339+CandersA@users.noreply.github.com>
2024-05-08 14:55:06 +0300

    Add blog route, add blog model the same as database

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
-app.get('/blog', async (req, res) => {
-    const blogPosts = await sequelize.query("SELECT * FROM blog", { type: QueryTypes.SELECT })
+class Blog extends Model {}
+Blog.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    author: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    url: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    title: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    likes: {
+        type: DataTypes.INTEGER,
+        default: 0
+    }
+}, {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog'
+})
+
+app.get('/api/blog', async (req, res) => {
+    const blogPosts = await Blog.findAll()
     res.json(blogPosts)
+})
+
+app.post('/api/blog', async (req, res) => {
+    try {
+        const blog = await Blog.create(req.body)
+        res.json(blog)
+    } catch (err) {
+        return res.status(400).json({ err })
+    }
 })
 
 const PORT = process.env.PORT || 3001
